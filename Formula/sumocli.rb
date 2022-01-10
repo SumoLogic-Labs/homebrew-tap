9f40413a447c94d101210b4d5a04e429971b3d2d
Kyle Jackson <58360154+wizedkyle@users.noreply.github.com>
2022-01-10 22:04:43 +1000

    Updating sumocli formula to 0.11.0 (#4)

--- Formula/sumocli.rb
+++ Formula/sumocli.rb
@@ -6,7 +6,7 @@ class Sumocli < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/SumoLogic-Labs/sumocli/releases/download/0.11.0/sumocli_0.11.0_darwin_amd64.tar.gz"
-      sha256 "5df548c673c2f35b6487116d74a443157e14321b0b097c6a57b493d655d58493"
+      sha256 "b8b7e2c277f3264f74ad994b241d2000579e92b60fd26b9b3b4b7e3976ed23c7"
 
       def install
         bin.install "sumocli"
@@ -15,7 +15,7 @@ class Sumocli < Formula
 
     if Hardware::CPU.arm?
       url "https://github.com/SumoLogic-Labs/sumocli/releases/download/0.11.0/sumocli_0.11.0_darwin_arm64.tar.gz"
-      sha256 "d73f4de4a4df75747d48ada516c06289087949fa4c175efc90bbfa1e73cc09d5"
+      sha256 "ce4c42b6775fcaf38b129f4ad4b09fd4f3ef224718d7901b0c08cabd9219988d"
 
       def install
         bin.install "sumocli"
@@ -26,7 +26,7 @@ class Sumocli < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/SumoLogic-Labs/sumocli/releases/download/0.11.0/sumocli_0.11.0_linux_amd64.tar.gz"
-      sha256 "968b5cb5441a2a961709c1e7322a7b979f92ac73528a5460cb1b2bbef0e8c632"
+      sha256 "356a7e157e22adfaccf95a0b3db709f769ab80ab5ed4e57195b03a5bc3b19850"
 
       def install
         bin.install "sumocli"
@@ -35,7 +35,7 @@ class Sumocli < Formula
 
     if Hardware::CPU.arm?
       url "https://github.com/SumoLogic-Labs/sumocli/releases/download/0.1.0/sumocli_0.11.0_linux_amd64.tar.gz"
-      sha256 "1655e843a72adb185c18b603e7abcc7eeef5d6b8bfac13979d3bc2f5f6a371da"
+      sha256 "f8a6cc404edc4c9475c24e0ffff3bc9f857c3c0ac485d9059c5fee442aef2cd7"
 
       def install
         bin.install "sumocli"
